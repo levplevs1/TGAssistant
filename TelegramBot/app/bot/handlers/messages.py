6ce1abe91f088a348531197b4333d92e6dac4b28
Lev Landon <dreeviclan@gmail.com>
2024-11-18 17:37:15 +0300

    Classifier Feature
    Добавлена функция, определяющая к какой категории относится запрос (вопрос/услуга)
    Задействуются 2 модели: малая и большая.

--- TelegramBot/app/bot/handlers/messages.py
+++ TelegramBot/app/bot/handlers/messages.py
@@ -1,7 +1,8 @@
 import threading
 from time import sleep
 from app.bot.handlers import waiting_for_response
-from llm.prompt_manager import process_user_request, include_headers
+from classifier.base_classifier import classify_is_type
+from llm.prompt_manager import process_user_request, include_headers_llm
 from utils.document_utils import search_by_header, extract_headers
 from utils.tokens import validate_count_tokens
 from config import bot
@@ -39,7 +40,7 @@ def texts(message):
         bot.send_message(message.chat.id, "Ваше сообщение слишком большое")
         return
 
-    if validate_count_tokens(message.text, 100):
+    if not validate_count_tokens(message.text, 100):
         memory = user_data.get('memory', [])  # Загружаем память пользователя
 
         for word in ["запомни", "сохрани", "не забудь","Запомни", "Сохрани", "Не забудь"]:
@@ -71,12 +72,29 @@ def texts(message):
 def handle_user_message(message):
     if message.text:
         user_id = message.from_user.id
-        headers = extract_headers(user_id)
-        llm_headers = include_headers(message.text, headers) # Извлечение заголовков LLM моделью
-        doc_text = search_by_header(llm_headers["commands"], user_id) # Поиск информации в документе по заголовкам
-        llm_answer = process_user_request(message.text, doc_text) # Формирование ответа LLM пользователю
-        bot.send_message(message.chat.id, llm_answer)
+
+        classify_type = classify_is_type(message.text)
+        # Тип запроса - услуга
+        if classify_type is True:
+            bot.send_message(message.chat.id, "Услуга")
+
+        # Тип запроса не определён
+        elif classify_type is None:
+            llm_answer = process_user_request(message.text, comment="Запрос пользователя не был определён. Уточните, что он(а) имел(а) ввиду")
+            bot.send_message(message.chat.id, llm_answer)
+
+        # Тип запроса - вопрос
+        else:
+            process_query(user_id, message)
+
         waiting_for_response[user_id] = False
+
+def process_query(user_id, message):
+    headers = extract_headers(user_id)  # Запрос является вопросом
+    llm_headers = include_headers_llm(message.text, headers)  # Извлечение заголовков LLM моделью
+    doc_text = search_by_header(llm_headers["commands"], user_id)  # Поиск информации в документе по заголовкам
+    llm_answer = process_user_request(message.text, doc_text)  # Формирование ответа LLM пользователю
+    bot.send_message(message.chat.id, llm_answer)
 
 def send_typing_action(chat_id, user_id):
     while waiting_for_response.get(user_id, False):
--- TelegramBot/tests/test_llm.py
+++ TelegramBot/tests/test_llm.py
@@ -1,8 +1,8 @@
-from llm.prompt_manager import process_user_request, include_headers
+from llm.prompt_manager import process_user_request, include_headers_llm
 
 def process_prompt(message):
     if message:
-        headers = include_headers(message)
+        headers = include_headers_llm(message)
 
         llm_answer = process_user_request(message)
         print(llm_answer)
